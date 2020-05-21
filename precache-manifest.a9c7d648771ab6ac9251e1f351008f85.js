b4ea8cb3f64cc75874803e35572cf066dcba4044
tootboi <danielyim12@gmail.com>
2020-05-21 22:42:49 +1200

    Updates

---
+++ precache-manifest.a9c7d648771ab6ac9251e1f351008f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d05818befdc1655c65cec59579b30e",
+    "url": "/React-ToDoApp/index.html"
+  },
+  {
+    "revision": "33aef806b43455462bf3",
+    "url": "/React-ToDoApp/static/css/main.c6190feb.chunk.css"
+  },
+  {
+    "revision": "ebcc2a0de3a6220c4bc4",
+    "url": "/React-ToDoApp/static/js/2.9f153dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDoApp/static/js/2.9f153dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33aef806b43455462bf3",
+    "url": "/React-ToDoApp/static/js/main.30d7f4bc.chunk.js"
+  },
+  {
+    "revision": "c7f84a9717dfd547a292",
+    "url": "/React-ToDoApp/static/js/runtime-main.0b60028d.js"
+  }
+]);
