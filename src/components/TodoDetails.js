789e325d6fe46f69459b3ef2c3b43654f8f58cfe
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-06-18 15:19:44 +1200

    todos now wrap properly

--- src/components/TodoDetails.js
+++ src/components/TodoDetails.js
@@ -2,26 +2,31 @@ import React, { useContext } from 'react';
 import { TodoContext } from '../contexts/TodoContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 import SubTodoList from './SubTodoList';
+import TodoOverlay from './TodoOverlay';
 
 const TodoDetails = ({todo}) => {
     const {dispatch} = useContext(TodoContext);
     const {isLightMode, light, dark} = useContext(ThemeContext);
     const theme = isLightMode ? light : dark;
+
+    const overlayOn = (e) => {
+        document.getElementById('overlay'+todo.id).style.display = 'block';
+    }
     return (
         <div className="todo" style={{ background: theme.bgColor, color: theme.todoColor}}>
-            <div className="mainTodo">
-                <span className="dragBtn"></span>
-                <input style={{ background: theme.bgColor, color: theme.todoColor}} 
-                    type="text" 
-                    className="dos" 
-                    onChange={(e) => dispatch({type: 'EDIT_TODO', 
-                        editTodo: {content: e.target.value, id: todo.id, subTodos: todo.subTodos}})}
-                    value={todo.content}
-                    onDoubleClick={() => dispatch({type: 'ADD_SUBTODO', id: todo.id})}
-                />
-                <span className="deleteBtn" onClick={() => dispatch({type: 'DELETE_TODO', id: todo.id})}>
-                    &times;
-                </span>
+            <div className="overlayContainer">
+                <div id={'overlay'+todo.id}>
+                    <TodoOverlay todo={todo}/>
+                </div>
+            </div>
+            <div className="mainTodo">     
+                <div className='todoContainer' style={{ background: theme.bgColor, color: theme.todoColor}} onDoubleClick={() => dispatch({type: 'ADD_SUBTODO', id: todo.id})}>
+                    <span className="dragBtn" onClick={overlayOn}></span>
+                    <span className='todoContent'>{todo.content}</span> 
+                    <span className="deleteBtn" onClick={() => dispatch({type: 'DELETE_TODO', id: todo.id})}>
+                        &times;
+                    </span>
+                </div>  
             </div>
             <SubTodoList subTodos={todo.subTodos} mainId={todo.id}/>
         </div>
