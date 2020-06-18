46b45fb6fe6b9a34208d806e4053966b5deb5f0b
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-06-18 15:32:01 +1200

    sub-todos now wrap properly

--- src/components/SubTodoDetails.js
+++ src/components/SubTodoDetails.js
@@ -1,24 +1,30 @@
 import React, { useContext } from 'react';
 import { TodoContext } from '../contexts/TodoContext';
 import { ThemeContext } from '../contexts/ThemeContext';
+import SubTodoOverlay from './SubTodoOverlay';
 
 const SubTodoDetails = ({subTodo, mainId}) => {
     const {dispatch} = useContext(TodoContext);
     const {isLightMode, light, dark} = useContext(ThemeContext);
     const theme = isLightMode ? light : dark;
+
+    const overlayOn = (e) => {
+        document.getElementById('overlay'+subTodo.id).style.display = 'block';
+    }
     return (
         <div className="subTodo">
-            <span className="dragBtn"></span>
-            <input style={{ background: theme.bgColor, color: theme.subColor}} 
-                type="text" className="dos" 
-                onChange={(e) => dispatch({type: 'EDIT_SUBTODO', editSubTodo: {content: e.target.value, id: subTodo.id, mainId: mainId}})} 
-                value={subTodo.content} 
-                autoFocus
-                onDoubleClick={() => dispatch({type: 'ADD_SUBTODO', id: mainId})}
-            />
-            <span className="deleteBtn" onClick={() => dispatch({type:'DELETE_SUBTODO', ids:{mainId: mainId, subId: subTodo.id}})}>
-                &times;
-            </span>
+            <div className="overlayContainer">
+                <div id={'overlay'+subTodo.id}>
+                    <SubTodoOverlay todo={subTodo} mainId={mainId}/>
+                </div>
+            </div>
+            <div className="subTodoContainer" style={{ background: theme.bgColor, color: theme.todoColor}} onDoubleClick={() => dispatch({type: 'ADD_SUBTODO', id: mainId})}>
+                <span className="dragBtn" onClick={overlayOn}></span>
+                <span className='todoContent'>{subTodo.content}</span> 
+                <span className="deleteBtn" onClick={() => dispatch({type:'DELETE_SUBTODO', ids:{mainId: mainId, subId: subTodo.id}})}>
+                    &times;
+                </span>
+            </div>
         </div>
     );
 }
